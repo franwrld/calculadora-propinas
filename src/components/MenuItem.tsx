0ef93c84fde199fba0b1e29c7966f4f1de02c349
franwrld <franwrld22@gmail.com>
2025-04-04 16:44:13 -0600

    Primer Commit CalculadoraPropinas

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+import type { MenuItems } from "../types"
+
+type MenuItemProps = {
+    item: MenuItems
+}
+
+export default function MenuItem({item} : MenuItemProps) {
+  return (
+    <>
+        <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between cursor-pointer">
+            <p>{item.name}</p>
+            <p className="font-black">$ {item.price}</p>
+        </button>
+        
+    </>
+  )
+}
