e7b8595b46809df865d65572d844263c711cf459
franwrld <franwrld22@gmail.com>
2025-04-07 09:19:24 -0600

    Funcion eliminar y agregar al consumo hecho

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export function formatCurrency(cantidad: number) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency', currency: 'USD'
+    }).format(cantidad)
+}
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,15 +1,29 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
-
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log('Agregando...')
+    const addItem = (item : MenuItem) => {
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist) {
+            const updatedOrder = order.map( orderItem => orderItem.id === item.id ?
+                {...orderItem, cantidad: orderItem.cantidad + 1}: orderItem 
+            )
+            setOrder(updatedOrder)
+        } else {
+            const newItem = {...item, cantidad: 1}
+            setOrder([...order, newItem])
+        }
+    }
+
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter(item => item.id !== id))
     }
 
     return {
-        addItem
+        order,
+        addItem,
+        removeItem
     }
 }
